cf7b0835dc390a4dd375bd21730bafd8ee2f1cb6
Michael <80940180+michael-freling@users.noreply.github.com>
2022-06-19 15:18:12 -0700

    fix: update config.fish to init anyenv and nodenv

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,10 @@ fish_add_path $HOME/.anyenv/bin
 # Load anyenv automatically by adding
 # the following to ~/.config/fish/config.fish:
 
-status --is-interactive; and source (anyenv init -|psub)
+status --is-interactive; and source (anyenv init - fish|psub)
 
 # Load nodenv automatically by appending
 # the following to ~/.config/fish/config.fish:
 
-status --is-interactive; and source (nodenv init -|psub)
+status --is-interactive; and source (nodenv init - fish|psub)
 
